c5558436eee4c608408a992e4e3810473ce10a40
Umang Agarwal <umang@Umangs-MacBook-Air.local>
2023-10-04 16:27:24 +0530

    Adds File/Folders

---
+++ linknsell/src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Loginpage(){
+    return (
+        <div>
+            <h1>Login Page Here</h1>
+        </div>
+    )
+}
---
+++ linknsell/src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignupPage(){
+    return (
+       <div>
+        <h1>Signup Page Here</h1>
+       </div> 
+    )
+}
